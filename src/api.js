269e724bbe89ede7887f08434c2b1328f1fa2955
Furkan Karakuş <116915007+m1erla@users.noreply.github.com>
2023-08-24 13:11:13 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,11 +2,11 @@
  export const geoApiOptions = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "e98ec3d7e4msh7cb9bb6b0482e67p1c97a8jsn893e5938ca09",// enter your rapid api key here
+      "X-RapidAPI-Key": "",// enter your rapid api key here
       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     },
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = "f67f5df8d7557f4e10221cb7a85b05a3"; // enter your key from openweather API
+  export const WEATHER_API_KEY = ""; // enter your key from openweather API
